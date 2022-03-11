d7db54238227bee3eab55b22168c773da67df797
Joshua Stoutenburg <jehoshua02@gmail.com>
2022-03-10 22:01:02 -0700

    Update Run Command

--- docker-run.sh
+++ docker-run.sh
@@ -8,8 +8,5 @@ docker run \
     --publish 27016:27016/udp \
     --publish 7777:7777/tcp \
     --publish 7777:7777/udp \
-    --rm \
     --volume `pwd`/Game.ini:/home/steam/longvinter-linux-server/Longvinter/Saved/Config/LinuxServer/Game.ini \
     jehoshua02/longvinter-linux-server:e97bae51c97dc1ef978761971932e9d91bbedecb
-
-docker logs -f longvinter:latest
